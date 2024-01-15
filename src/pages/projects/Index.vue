f351b570f4ed91c97f3db894aee005c04ba57b27
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-15 15:12:20 +0100

    Print to console.log results from axios call

--- src/pages/projects/Index.vue
+++ src/pages/projects/Index.vue
@@ -1,4 +1,25 @@
 <script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        projects: [],
+        BASE_URL: 'http://127.0.0.1:8000/api',
+      }
+    },
+    methods: {
+      fetchProjects() {
+        axios.get(`${this.BASE_URL}/projects`)
+        .then((res) => {
+          console.log(res)
+        })
+      }
+    },
+    created() {
+      this.fetchProjects()
+    }
+  }
 </script>
 
 <template>
