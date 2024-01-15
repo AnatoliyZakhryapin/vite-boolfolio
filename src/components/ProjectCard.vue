304e3ab26dba5620ea8dfe741061d85c0d70afb9
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-15 16:47:35 +0100

    ADD all information of project to card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -11,11 +11,18 @@ export default {
 </script>
 
 <template>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+    <div class="card h-100">
+        <img src="https://picsum.photos/300/200" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ project.title }}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">{{ project.slug }}</p>
+            <p v-if="project.type">{{  project.type.name }}</p>
+            <p class="d-flex gap-1 flex-wrap">
+                <span  v-for="tecnology in project.tecnologies">{{ tecnology.name }}</span>
+            </p>
+            <p class="card-text">{{ project.description }}</p>
+            <p class="card-text">{{ project.user.name }}</p>
+            <p class="card-text"><a href="#">{{ project.url }}</a></p>
         </div>
     </div>
 </template>
--- src/pages/projects/Index.vue
+++ src/pages/projects/Index.vue
@@ -6,6 +6,8 @@
     data() {
       return {
         projects: [],
+        currentPage: [],
+        lastPage: [],
         BASE_URL: 'http://127.0.0.1:8000/api',
       }
     },
@@ -17,8 +19,12 @@
         axios.get(`${this.BASE_URL}/projects`)
         .then((res) => {
           console.log(res)
-          this.projects = res.data.results
+          this.projects = res.data.results.data
+          this.currentPage = res.data.results.current_page
+          this.lastPage = res.data.results.last_page
           console.log(this.projects)
+          console.log(this.currentPage)
+          console.log(this.lastPage)
         })
       }
     },
@@ -36,9 +42,15 @@
         <h1>Projects</h1>
       </div>
     </div>
-    <div class="row">
-      <div class="col" >
-        <ProjectCard v-for="project in projects" :project="project"/>
+    <div class="row mb-5">
+      <div class="col">
+        <p><strong>CurrentPage:</strong> {{ currentPage }}</p>
+        <p><strong>LastPage:</strong> {{ lastPage }}</p>
+      </div>
+    </div>
+    <div class="row row-gap-5">
+      <div class="col-6" v-for="project in projects">
+        <ProjectCard :project="project"/>
       </div>
     </div>
   </div>
