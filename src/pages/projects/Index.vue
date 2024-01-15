2141a42e978bdc2d32b40af26746beafbaca46de
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-15 14:54:19 +0100

    first commit

---
+++ src/pages/projects/Index.vue
@@ -0,0 +1,18 @@
+<script>
+import ProjectIndex from './pages/projects/Index.vue';
+  export default {
+    components: {
+      ProjectIndex,
+    }
+
+  }
+</script>
+
+<template>
+  <ProjectIndex/>
+</template>
+
+<style lang="scss">
+
+
+</style>
