d8b32f3907cf16a41b7e298100401e95fb9a0637
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-16 14:15:28 +0100

    ADD HomePage.vue

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "Anatoliy Zakhryapin",
+            jobtitle: 'Junior Full Stack Developer'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+    <h2>{{ jobtitle }}</h2>
+</template>
+
+<style lang="scss">
+</style>
