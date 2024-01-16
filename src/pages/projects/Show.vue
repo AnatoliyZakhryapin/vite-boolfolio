28456b97906fc02d3293614c8fa10cc99572da6a
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-16 15:49:48 +0100

    ADD v-if = 'project' - for dispay after return response

--- src/pages/projects/Show.vue
+++ src/pages/projects/Show.vue
@@ -24,31 +24,32 @@ export default {
 </script>
 
 <template>
-    <h5 class="card-title">Titolo: </h5>
-    <div class="container">
-        <div class="row row-gap-5 justify-content-center">
-            <div class="col-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Titolo: {{ project.title }}</h5>
-                        <p class="card-text">Slug:{{ project.slug}}</p>
-                        <p v-if="project.type">
-                            <strong>
-                            {{ project.type.name }}
-                            </strong>
-                        </p>
+    <div v-if="project">
+        <div class="container">
+            <div class="row row-gap-5 justify-content-center">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Titolo: {{ project.title }}</h5>
+                            <p class="card-text">Slug:{{ project.slug}}</p>
+                            <p v-if="project.type">
+                                <strong>
+                                {{ project.type.name }}
+                                </strong>
+                            </p>
 
-                        <ul class="d-flex gap-2">
-                            <li class="badge rounded-pill text-bg-primary" v-for="tecnology in project.tecnologies">
-                                {{ tecnology.name }}
-                            </li>
-                        </ul>
+                            <ul class="d-flex gap-2">
+                                <li class="badge rounded-pill text-bg-primary" v-for="tecnology in project.tecnologies">
+                                    {{ tecnology.name }}
+                                </li>
+                            </ul>
 
-                        <p class="card-text">Descrizione: {{ project.description}}</p>
-                        <p class="card-text">Creato da: {{ project.user.name}}</p>
-                        <p class="card-text">Link: {{ project.url}}</p>
-                        <p class="card-text">Data: {{ project.created_at}}</p>
-                        <p class="card-text">Aggiornato: {{ project.updated_at}}</p>
+                            <p class="card-text">Descrizione: {{ project.description}}</p>
+                            <p class="card-text">Creato da: {{ project.user.name}}</p>
+                            <p class="card-text">Link: {{ project.url}}</p>
+                            <p class="card-text">Data: {{ project.created_at}}</p>
+                            <p class="card-text">Aggiornato: {{ project.updated_at}}</p>
+                        </div>
                     </div>
                 </div>
             </div>
