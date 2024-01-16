99857f7beaaf779982c649d39982883f3b44088f
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-16 15:09:45 +0100

    ADD new route for Sho.vue

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePageVue from './pages/HomePage.vue';
 import ProjectsIndex from './pages/projects/Index.vue';
+import ProjectsShow from './pages/projects/Show.vue';
 import Contacts from './pages/Contacts.vue';
 
 const router = createRouter({
@@ -16,6 +17,11 @@ const router = createRouter({
             path: '/portfolio',
             name: 'projects.index',
             component: ProjectsIndex,
+        },
+        {
+            path: '/portfolio/:slug',
+            name: 'projects.show',
+            component: ProjectsShow,
         },
         {
             path: '/contacts',
