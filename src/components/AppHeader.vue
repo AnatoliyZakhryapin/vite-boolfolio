b79466d90818f8ab7db44a672d45df9c7d97cb4d
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-16 14:10:21 +0100

    ADD AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<template>
+    <h1>Header</h1>
+    <ul>
+        <li>
+            <router-link :to="{ name: 'home'}">Home</router-link>
+        </li>
+        <li>
+            <router-link :to="{ name: 'projects.index'}">Portfolio</router-link>
+        </li>
+        <li>
+            <router-link :to="{ name: 'contacts'}">Contatti</router-link>
+        </li>
+    </ul>
+</template>
+
+<style lang="scss">
+
+
+</style>
