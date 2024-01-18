7c8488a00e93d93c0c81605367cf628bb6c84421
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-18 09:28:32 +0100

    INSTALL FontAwesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import { router } from './router'
 
-createApp(App).use( router ).mount('#app')
+import { 
+	faTableCellsLarge as fasTableCellsLarge,
+    faTableCells as fasTableCells,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+    fasTableCellsLarge,
+    fasTableCells,
+	)
+
+createApp(App).use( router ).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
