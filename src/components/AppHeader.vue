9e43ca7597036fc6834e00e12524ca7212549df5
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-27 15:09:23 +0100

    ADD style to nav-menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,21 +2,67 @@
 </script>
 
 <template>
-    <h1>Header</h1>
-    <ul>
-        <li>
-            <router-link :to="{ name: 'home'}">Home</router-link>
-        </li>
-        <li>
-            <router-link :to="{ name: 'projects.index'}">Portfolio</router-link>
-        </li>
-        <li>
-            <router-link :to="{ name: 'contacts'}">Contatti</router-link>
-        </li>
-    </ul>
+    <header class="page-header py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <nav>
+                        <ul class="nav-menu">
+                            <li class="menu-item">
+                                <router-link :to="{ name: 'home'}">Home</router-link>
+                            </li>
+                            <li class="menu-item">
+                                <router-link :to="{ name: 'projects.index'}">Progetti</router-link>
+                            </li>
+                            <li class="menu-item">
+                                <router-link :to="{ name: 'contacts'}">Contatti</router-link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
+    .page-header{
+        position: fixed;
+        width: 100%;
+        z-index: 1;
+    }
+    .nav-menu {
+        display: flex;
+        gap: 30px;
+        list-style: none;
+        font-size: 30px;
+        & .menu-item{
+            a {
+                text-decoration: none;
+                color: currentColor;
+            };
+        };
 
+        li:hover{
+            color: #f1f0f0;
+            scale: 1.2;
+            
+        }
+
+        li a.router-link-active{
+            text-shadow: 0px 0px 26px  rgba(0, 255, 0, 0.938);
+            color:  rgba(0, 255, 0, 0.938);
+
+            &::after{
+                content: "";
+                display: block;
+                width: 100%;
+                height: 2px;
+                background-color:  rgba(0, 255, 0, 0.938);
+                box-shadow: 0px 0px 26px  rgba(0, 255, 0, 0.938);
+                margin: 0 auto;
+            }
+        }
+    }
 </style>
